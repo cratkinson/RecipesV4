e955fcb2d13f83a885cc5c32ec3260b68b700446
cratkinson <chip@atkinsons.com>
2013-10-21 16:06:09 -0600

    Add some Units

--- RecipeTester/Form1.vb
+++ RecipeTester/Form1.vb
@@ -3,8 +3,11 @@ Public Class Form1
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         Dim theApp As RecipeLibrary.App = New RecipeLibrary.App
-        theApp.Category_Insert(New Category With {.Description = "Soups"})
-        theApp.Category_Insert(New Category With {.Description = "Desserts"})
+        With theApp
+            .Unit_Insert(New Unit With {.Description = "tbl", .ExpandedDescription = "tablespoon"})
+            .Unit_Insert(New Unit With {.Description = "cup", .ExpandedDescription = ""})
+            .Unit_Insert(New Unit With {.Description = "c", .ExpandedDescription = "cup"})
+        End With
         theApp.Save()
 
     End Sub
